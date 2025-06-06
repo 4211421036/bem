849542c08b68b04b4d6b82ef8a4c5226af184e56
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-06 10:10:49 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="plasmonmode",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        'numpy>=1.20.0',
-        'scipy>=1.7.0',
-    ],
-    author="GALIH RIDHO UTOMO",
-    author_email="g4lihru@students.unnes.ac.id",
-    description="Python implementation of plasmon eigenmodes computation",
+    name='plasmonmode', # Beri nama unik untuk modul Anda di PyPI
+    version='0.1.0',
+    author='GALIH RIDHO UTOMO',
+    author_email='g4lihru@students.unnes.ac.id',
+    description='A Python module for plasmon mode calculations, converted from MATLAB.',
     long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/4211421036/bem",
+    long_description_content_type='text/markdown',
+    url='https://github.com/4211421036/bem',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', # Atau lisensi lain yang Anda inginkan
+        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.7', # Sesuaikan dengan versi Python yang Anda targetkan
+    install_requires=[
+        'numpy',
+        'scipy', # Tambahkan ini jika Anda akan menggunakan SciPy untuk bemstateig
+    ],
 )
